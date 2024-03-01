7b0ad0873b0f2cdd9aee81a1b998a66fdeb8bdaa
mtzw <mail@mtzw.jp>
2024-03-01 10:26:39 +0900

    GitHub Pages

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="p-4">
+      <div className="my-2">
+        <h1 className="text-2xl">mtzw.jp</h1>
+      </div>
+      <ul>
+        <li><a href="http://github.com/mtzw" className="no-underline hover:underline">github/mtzw</a></li>
+      </ul>
+    </div>
+  );
+}
